6273be6b905bca5506f3ceeba62a5585c6025cbc
Lokode <10hlearns@gmail.com>
2018-12-15 18:25:19 -0500

    carousel commit

---
+++ src/components/featured/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Carrousel from './Carrousel';
+
+
+const Featured = () => {
+    return ( <
+        div style = {
+            { position: 'relative' } } >
+
+        <
+        Carrousel / >
+
+        <
+        div className = "artist_name" >
+        <
+        div className = "wrapper" >
+        RÜFÜS DU SOL <
+        /div> <
+        /div>
+
+
+        <
+        /div>
+    );
+};
+
+export default Featured;
