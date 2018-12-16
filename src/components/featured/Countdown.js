d066043a8d88668ad8f2db3d557ae991b2f30030
Lokode <10hlearns@gmail.com>
2018-12-15 21:05:39 -0500

    countdown-test commit

---
+++ src/components/featured/Countdown.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Countdown extends Component {
+    state ={
+            
+    }
+
+    render() {
+        
+        return (
+            <div className="countdown_wrapper">
+                <div className="countdown_top">
+                    Show starts in
+                </div>
+                
+            </div>
+        );
+    }
+}
+
+export default Countdown;
